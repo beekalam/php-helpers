32003716bbe5805c8201dd302ace5e2ee43a686b
Mohammad Reza mansouri <beekalam@gmail.com>
2021-12-01 22:10:08 +0330

    IO

---
+++ src/IO.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Beekalam\PHPHelpers;
+class IO{
+    public static function stdin_stream()
+    {
+        while($line = fgets(STDIN))
+        {
+            yield $line;
+        }
+    }
+
+    public static function read_int_array()
+    {
+        $str = fgets(STDIN);
+        $arr = explode(" ", $str);
+        $arr = array_map(function ($in) {
+            return (int)$in;
+        }, $arr);
+        return $arr;
+    }
+}
